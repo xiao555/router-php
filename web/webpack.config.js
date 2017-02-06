6f82e6886c938173363d944da19d8175844b9573
xiao555 <zhangruiwu32@gmail.com>
2017-02-06 21:15:29 +0800

    fix html render & fix img loader

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,6 +3,7 @@ var path                = require('path');
 var glob                = require('glob');
 var webpack             = require('webpack');
 var poststylus          = require('poststylus');
+var stylusUrl           = require('stylus-url');
 var ExtractTextPlugin   = require("extract-text-webpack-plugin");
 var BrowserSyncPlugin   = require('browser-sync-webpack-plugin');
 var HtmlWebpackPlugin   = require('html-webpack-plugin');
@@ -29,9 +30,9 @@ var config = {
     main: 'scripts', // alias, realpath is ./src/scripts/index.js
   },
   output: {
-    path: 'build',
-    filename: "assets/[name].js",
-    chunkFilename: "assets/[id].js"
+    path: 'build/assets',
+    filename: "[name].js",
+    chunkFilename: "[id].js"
   },
   module: {
     loaders: [
@@ -40,12 +41,6 @@ var config = {
       { test: /\.(woff|woff2)$/, loader: 'file-loader?name=static/[hash].[ext]'},
       { test: /\.(ttf|eot|otf)$/, loader: 'file-loader?name=static/[hash].[ext]'},
       { test: /\.txt$/, loader: 'raw-loader'},
-      { test: /\.html$/, loader: 'file-loader?name=[name].html!nunjucks-html-loader?' +
-                JSON.stringify({
-                    'searchPaths': [
-                        'views'
-                    ]
-                }) },
       // Extract css files
       { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap") },
       // Optionally extract less files
@@ -55,12 +50,14 @@ var config = {
     ]
   },
   externals: {
-    jquery: 'jQuery'
+    jquery: 'jQuery',
+    window: 'window',
+    document: 'document'
   },
   resolve: {
-    root: path.join(__dirname, './'),
+    // root: path.join(__dirname, './src'),
     modulesDirectories: ['node_modules', 'bower_components'],
-    extensions: ['', '.es6.js', '.js', '.vue'],
+    extensions: ['', '.es6.js', '.js', '.vue' ],
     alias: {
       'assets': paths.assets,
       'styles': paths.styles,
@@ -71,12 +68,15 @@ var config = {
   },
   resolveLoader: {
     alias: {
-      'copy': 'file-loader?name=assets/[name].[ext]', //&context=./src
+      'copy': 'file-loader?name=[name].[ext]', //&context=./src
     }
   },
   // User Custom Config
   stylus: {
     use: [
+      stylusUrl({
+        root: paths.src
+      }),
       poststylus([ 'autoprefixer' ]),
     ],
     import: [
@@ -94,7 +94,7 @@ var config = {
     new webpack.ProvidePlugin({
       $: 'jquery'
     }),
-    new ExtractTextPlugin("assets/[name].css"),
+    new ExtractTextPlugin("[name].css"),
     new BrowserSyncPlugin(
       // BrowserSync options 
       {
@@ -120,27 +120,15 @@ var config = {
   ]
 };
 
-var pages = Object.keys(getEntry('views/*.html', 'build/'));
+var pages = Object.keys(getEntry('views/**/*.html', ['includes', 'base']));
 pages.forEach(function(pathname) {
     var conf = {
-        filename: 'build/' + pathname + '.html', //生成的html存放路径，相对于path
-        template:  pathname + '.html', //html模板路径
-        inject: false,    //js插入的位置，true/'head'/'body'/false
-        /*
-        * 压缩这块，调用了html-minify，会导致压缩时候的很多html语法检查问题，
-        * 如在html标签属性上使用{{...}}表达式，所以很多情况下并不需要在此配置压缩项，
-        * 另外，UglifyJsPlugin会在压缩代码的时候连同html一起压缩。
-        * 为避免压缩html，需要在html-loader上配置'html?-minimize'，见loaders中html-loader的配置。
-         */
-        // minify: { //压缩HTML文件
-        //     removeComments: true, //移除HTML中的注释
-        //     collapseWhitespace: false //删除空白符与换行符
-        // }
+        template:  '!!file-loader?name=../' + pathname + '.html!nunjucks-html-loader?{\"searchPaths\":[\"views\"]}!views/' + pathname + '.html', //html模板路径
     };
     config.plugins.push(new HtmlWebpackPlugin(conf));
 });
 
-function getEntry(globPath, pathDir) {
+function getEntry(globPath, filter) {
     var files = glob.sync(globPath);
     var entries = {},
         entry, dirname, basename, pathname, extname;
@@ -148,11 +136,18 @@ function getEntry(globPath, pathDir) {
     for (var i = 0; i < files.length; i++) {
         entry = files[i];
         dirname = path.dirname(entry); // views/
+        var father = dirname.split('/').reverse();
+        if (filter.indexOf(father[0]) !== -1) continue;
         extname = path.extname(entry); // [.ext]
         basename = path.basename(entry, extname);
+        if (filter.indexOf(basename) !== -1) continue;
         pathname = path.join(dirname, basename);
-        pathname = pathDir ? pathname.replace(new RegExp('^' + pathDir), '') : pathname;
-        entries[pathname] = ['./' + entry];
+        var tararr = pathname.split('/');
+        tararr.shift();
+        console.log(tararr);
+        var target = tararr.join('/');
+        console.log(target);
+        entries[target] = ['./' + entry];
     }
     return entries;
 }
